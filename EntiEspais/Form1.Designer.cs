0492189f839986aed42b121fee2ccf05a3f43810
miquelpuente <mipuentema@gmail.com>
2019-02-19 19:43:26 +0100

    Añadimos planilla para las futuras ventanas

--- EntiEspais/Form1.Designer.cs
+++ EntiEspais/Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonMinimizeApp = new System.Windows.Forms.Button();
+            this.buttonCloseApp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonMinimizeApp
+            // 
+            this.buttonMinimizeApp.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.buttonMinimizeApp.FlatAppearance.BorderSize = 0;
+            this.buttonMinimizeApp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMinimizeApp.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMinimizeApp.Image = global::EntiEspais.Properties.Resources.minus;
+            this.buttonMinimizeApp.Location = new System.Drawing.Point(781, 12);
+            this.buttonMinimizeApp.Name = "buttonMinimizeApp";
+            this.buttonMinimizeApp.Size = new System.Drawing.Size(25, 23);
+            this.buttonMinimizeApp.TabIndex = 1;
+            this.buttonMinimizeApp.UseVisualStyleBackColor = true;
+            this.buttonMinimizeApp.Click += new System.EventHandler(this.buttonMinimizeApp_Click);
+            // 
+            // buttonCloseApp
+            // 
+            this.buttonCloseApp.FlatAppearance.BorderSize = 0;
+            this.buttonCloseApp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCloseApp.Image = global::EntiEspais.Properties.Resources.multiply;
+            this.buttonCloseApp.Location = new System.Drawing.Point(812, 12);
+            this.buttonCloseApp.Name = "buttonCloseApp";
+            this.buttonCloseApp.Size = new System.Drawing.Size(25, 23);
+            this.buttonCloseApp.TabIndex = 0;
+            this.buttonCloseApp.UseVisualStyleBackColor = true;
+            this.buttonCloseApp.Click += new System.EventHandler(this.buttonCloseApp_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(117)))), ((int)(((byte)(222)))), ((int)(((byte)(213)))));
+            this.ClientSize = new System.Drawing.Size(849, 507);
+            this.Controls.Add(this.buttonMinimizeApp);
+            this.Controls.Add(this.buttonCloseApp);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registrar-se";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonCloseApp;
+        private System.Windows.Forms.Button buttonMinimizeApp;
     }
 }
 
--- EntiEspais/Form1.cs
+++ EntiEspais/Form1.cs
@@ -10,11 +10,55 @@ using System.Windows.Forms;
 
 namespace EntiEspais
 {
-    public partial class Form1 : Form
+    public partial class Form1 : System.Windows.Forms.Form
     {
+        // Atributs per a moure la finestra
+        private bool dragging = false;
+        private Point dragCursorPoint;
+        private Point dragFormPoint;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonCloseApp_Click(object sender, EventArgs e)
+        {
+            // Botó per a tancar l'aplicació
+            Application.Exit();
+
+            // Botó per a tancar només la finestra (Comentar el métode d'adalt)
+            //this.Close();
+        }
+
+        private void buttonMinimizeApp_Click(object sender, EventArgs e)
+        {
+            // Botó per a minimitzar la finestra
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Sirve para mover la ventana (1 - Cuándo haces clic y mantienes) 
+            dragging = true;
+            dragCursorPoint = Cursor.Position;
+            dragFormPoint = this.Location;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Sirve para mover la ventana (2 - Cuándo arrastras el ratón)
+            if (dragging)
+            {
+                Point dif = Point.Subtract(Cursor.Position, new Size(dragCursorPoint));
+                this.Location = Point.Add(dragFormPoint, new Size(dif));
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            // Sirve para mover la ventana (3 - Cuando sueltas el botón de clik)
+            dragging = false;
         }
     }
 }
