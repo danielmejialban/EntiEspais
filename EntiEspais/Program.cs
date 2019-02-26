9d816083d9e077d9d13362a327ed9661fd55ca63
miquelpuente <mipuentema@gmail.com>
2019-02-26 18:53:06 +0100

    Añadimos form iniciar sesión (Todo programado)

--- EntiEspais/Program.cs
+++ EntiEspais/Program.cs
@@ -16,7 +16,7 @@ namespace EntiEspais
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Registre());
         }
     }
 }
